a59c45c8dd3efdb481b86f1788362c551202fcd3
Jaki Riggs <jarquealex0302@gmail.com>
2026-05-16 19:45:19 +0200

    feat: first changes for final landing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from "next";
+import { Bebas_Neue } from 'next/font/google'
 import './globals.css';
 
 export const metadata: Metadata = {
   title: "G.O.A.T.",
   description: "Luthier Barcelona",
 };
+
+const bebasNeue = Bebas_Neue({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-bebas',
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +21,7 @@ export default function RootLayout({
   return (
     <html
       lang="es"
-      className={`h-full antialiased`}
+      className={`h-full antialiased ${bebasNeue.variable}`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,11 +1,14 @@
-import Image from 'next/image';
 
 function Landing() {
     return (
-        <div className="bg-black h-screen">
-            <Image src="/img/logo_white_noletters.png" alt="" width={300} height={300}></Image>
-            <div>Luthier en Gava</div>
-            <a href="#contact">Contacto</a>
+        <div className="h-screen bg-cover bg-[center_00%]"
+            style={{backgroundImage: "url('/img/landingv.jpg')" }}>
+                <div className="bg-gray-900/50 w-full h-full">
+                    <div className="flex justify-center items-center flex-col gap-20 text-white text-center w-1/3 h-full mx-auto">
+                        <div className="text-[5.5rem] font-bebas mt-10">REPARACIÓN PROFESIONAL DE INSTRUMENTOS DE CUERDA</div>
+                        <a className="bg-black text-7xl font-bebas" href="#contact">Contacto</a>
+                    </div> 
+                </div>
         </div>
     )
 }
