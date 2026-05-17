4c27ca98a856d9e6cc62b26115958b9553ca1910
Jaki Riggs <jarquealex0302@gmail.com>
2026-05-17 11:05:03 +0200

    feat: title from landing adapted

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,11 +3,9 @@ function Landing() {
     return (
         <div className="h-screen bg-cover bg-[center_00%]"
             style={{backgroundImage: "url('/img/landingv.jpg')" }}>
-                <div className="bg-gray-900/50 w-full h-full">
-                    <div className="flex justify-center items-center flex-col gap-20 text-white text-center w-1/3 h-full mx-auto">
-                        <div className="text-[5.5rem] font-bebas mt-10">REPARACIÓN PROFESIONAL DE INSTRUMENTOS DE CUERDA</div>
-                        <a className="bg-black text-7xl font-bebas" href="#contact">Contacto</a>
-                    </div> 
+                <div className="bg-gray-900/50 w-full h-full flex justify-center items-center flex-col text-white text-center w-1/3 h-full mx-auto">
+                    <div className=" flex justify-center items-center flex-col text-white text-center w-1/3 h-full mx-auto text-[5.6rem] font-bebas mt-10 leading-none">REPARACIÓN PROFESIONAL DE INSTRUMENTOS DE CUERDA</div>
+                    <a className="bg-black text-7xl font-bebas mb-14" href="#contact">Contacto</a>
                 </div>
         </div>
     )
