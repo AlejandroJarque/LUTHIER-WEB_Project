ccca8b1d83d1d8815ac9adc91a17b2b559b82f18
Jaki Riggs <jarquealex0302@gmail.com>
2026-05-19 12:21:26 +0200

    feat: finnal Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -4,8 +4,8 @@ function Landing() {
         <div className="h-screen bg-cover bg-[center_00%]"
             style={{backgroundImage: "url('/img/landingv.jpg')" }}>
                 <div className="bg-gray-900/50 w-full h-full flex justify-center items-center flex-col text-white text-center w-1/3 h-full mx-auto">
-                    <div className=" flex justify-center items-center flex-col text-white text-center w-1/3 h-full mx-auto text-[5.6rem] font-bebas mt-10 leading-none">REPARACIÓN PROFESIONAL DE INSTRUMENTOS DE CUERDA</div>
-                    <a className="bg-black text-7xl font-bebas mb-14" href="#contact">Contacto</a>
+                    <div className=" flex justify-center items-center flex-col text-white text-center w-1/3 h-full mx-auto text-[5.6rem] font-bebas mt-14 leading-[1.01]">REPARACIÓN PROFESIONAL DE INSTRUMENTOS DE CUERDA</div>
+                    <a className="bg-black text-7xl font-bebas mb-14 p-3 pb-1 pt-2 rounded-lg border-2 border-white" href="#contact">Contacto</a>
                 </div>
         </div>
     )
