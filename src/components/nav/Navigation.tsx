c26f03a03d893318a1cd49adba9fe2f1a0fb18b9
brainight <lordstoneddog@gmail.com>
2026-05-25 17:10:23 +0200

    Added JukkaLogo SVG

--- src/components/nav/Navigation.tsx
+++ src/components/nav/Navigation.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image"
 import Link from "next/link"
 import MobileMenu from "./MobileMenu"
-import { JukkaIcon } from "../svg/SVGIcons"
+import { JukkaLogo } from "../svg/SVGIcons"
 // bg-linear-to-b from-jukka-dark/90 from-20% to-jukka-dark/10
 export default function Navigation({ i18 }: any) {
     return <>
-        <header className="fixed w-full flex flex-row justify-between sm:justify-center items-center md:h-14 h-18 bg-linear-to-b from-jukka-dark/90 from-20% to-jukka-dark/10">
+        <header className="fixed w-full flex flex-row justify-between sm:justify-center items-center md:h-16 h-18 bg-linear-to-b from-jukka-dark/90 from-20% to-jukka-dark/10">
             <a href="/" className="flex flex-row ml-4">
                 {/* <Image src="/img/logo_white_noletters.png" width={40} height={40} alt="F***" className="ml-6" /> */}
-                <JukkaIcon className="flex w-10 h-10 hover:text-jukka-blood hover:duration-500 duration-500" />
+                <JukkaLogo className="flex w-12 h-12 hover:text-jukka-blood hover:duration-500 duration-500" />
             </a>
             <nav className="w-full hidden sm:flex flex-col items-center justify-center">
                 <ul className="flex flex-wrap list-none justify-center gap-4">
