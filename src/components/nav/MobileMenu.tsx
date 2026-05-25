87ef7544b6eb3aaa5f4da101e986d2f1f2d89168
brainight <lordstoneddog@gmail.com>
2026-05-25 16:53:35 +0200

    Change png logo to svg in navigation

--- src/components/nav/MobileMenu.tsx
+++ src/components/nav/MobileMenu.tsx
@@ -11,7 +11,7 @@ export default function MobileMenu({ i18 }: any) {
 
     return <>
         <button onClick={() => setOpen(!open)}
-            className="p-4"><BurgerIcon className="w-8 h-8" /></button>
+            className="p-4 cursor-pointer hover:duration-500 duration-500 hover:text-jukka-blood"><BurgerIcon className="w-8 h-8" /></button>
 
         <nav className={`fixed top-0 right-0 w-full h-full bg-jukka-dark/100 flex flex-col gap-10 items-center justify-around
              transition-all duration-600  ease-in-out ${open ? 'z-50  translate-y-0 visible opacity-98' : 'z-0 -translate-y-full invisible opacity-30 delay-400 '}`}>
@@ -23,8 +23,8 @@ export default function MobileMenu({ i18 }: any) {
                 <li className={`px-7 py-10 hover:text-jukka-blood hover:duration-500 duration-500 text-5xl transition-all duration-600  ease-in-out ${open ? 'translate-x-0 delay-300' : 'translate-x-[200%] delay-300'}`}><Link href="#services" onClick={() => setOpen(!open)}>{i18.services}</Link></li>
                 <li className={`px-7 py-10 hover:text-jukka-blood hover:duration-500 duration-500 text-5xl transition-all duration-600  ease-in-out ${open ? 'translate-x-0 delay-600' : 'translate-x-[200%] delay-100'}`}><Link href="#contact" onClick={() => setOpen(!open)}>{i18.contact}</Link></li>
             </ul>
-            <button onClick={() => setOpen(!open)}>
-                <CloseIcon className="w-8 h-8" />
+            <button onClick={() => setOpen(!open)} className="cursor-pointer hover:duration-500 duration-500 hover:text-jukka-blood">
+                <CloseIcon className="w-12 h-12 " />
             </button>
         </nav>
 
